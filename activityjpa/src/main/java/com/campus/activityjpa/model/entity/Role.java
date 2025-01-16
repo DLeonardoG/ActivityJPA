c628636fcd9e25d630f8becb96710a06eddca3cd
DLeonardoG <darkleonardog@gmail.com>
2025-01-16 13:56:41 -0500

    perf: :zap: Edited Role
    Not finished yet
    
    By @DLeonardoG

--- activityjpa/src/main/java/com/campus/activityjpa/model/entity/Role.java
+++ activityjpa/src/main/java/com/campus/activityjpa/model/entity/Role.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.campus.activityjpa.model.entity;
 
-/**
- *
- * @author camper
- */
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
 public class Role {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String role;
+    
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<CrewMember> members = new ArrayList<>();
+    
     
 }
