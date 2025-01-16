4084d87b821a34943490d39783eb8f5e60620117
DLeonardoG <darkleonardog@gmail.com>
2025-01-16 12:21:17 -0500

    feat: :tada: The project was started
    Just with the spring project and Readme
    
    By @DLeonardoG

---
+++ activityjpa/src/main/java/com/campus/activityjpa/ActivityjpaApplication.java
@@ -0,0 +1,13 @@
+package com.campus.activityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityjpaApplication.class, args);
+	}
+
+}
