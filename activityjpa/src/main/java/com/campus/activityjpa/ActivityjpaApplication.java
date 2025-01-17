f8ade20d3243e490dcaa6a1a14da92c345049029
Santiago-SanP05 <santiagopinzoncruz@gmail.com>
2025-01-16 20:43:17 -0500

    feat: :zap: The constructor of some tables has been introduced
    Se ha introducido el constructor de las tablas "Maintenance", "Passanger", "Payment"
    
    By @Santiago-SanP05

--- activityjpa/src/main/java/com/campus/activityjpa/ActivityjpaApplication.java
+++ activityjpa/src/main/java/com/campus/activityjpa/ActivityjpaApplication.java
@@ -8,6 +8,7 @@ public class ActivityjpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActivityjpaApplication.class, args);
+                System.out.println("hola mundo");
 	}
 
 }
--- activityjpa/src/main/java/com/campus/activityjpa/model/entity/Passanger.java
+++ activityjpa/src/main/java/com/campus/activityjpa/model/entity/Passanger.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.campus.activityjpa.model.entity;
 
-/**
- *
- * @author camper
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Passanger {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long idPassanger;
+    private String name;
+    private int ID;
     
 }
