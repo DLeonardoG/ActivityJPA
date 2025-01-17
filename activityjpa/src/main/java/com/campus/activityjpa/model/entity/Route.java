86641616688b797545c0cf3c3969644ded18cafd
Kevinromero04 <kevinromerorincon7@gmail.com>
2025-01-16 21:14:05 -0500

    feat: :sparkles: Creation of attributes
    Were created the attributes in the classes Route, Ticket and TypeMaintenance
    
    By @Kevinromero04

--- activityjpa/src/main/java/com/campus/activityjpa/model/entity/Route.java
+++ activityjpa/src/main/java/com/campus/activityjpa/model/entity/Route.java
@@ -4,10 +4,106 @@
  */
 package com.campus.activityjpa.model.entity;
 
-/**
- *
- * @author camper
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Route {
+    
+    private Long IdRoute;
+    
+    private String codeRoute;
+    
+    private String cityOrigin;
+    
+    private String cityDestine;
+    
+    private double distance;
+    /*
+    @ManyToOne
+    @JoinColumn(name = "Id_Airport_Origin", nullable = false)
+    private Airport idAirportOrigin;
+    
+    @ManyToOne
+    @JoinColumn(name = "Id_Airport_Destine", nullable = false)
+    private Airport idAirportDestine;
+    */
+    public Route() {
+    }
+
+    public Route(Long IdRoute, String codeRoute, String cityOrigin, String cityDestine, double distance) {
+        this.IdRoute = IdRoute;
+        this.codeRoute = codeRoute;
+        this.cityOrigin = cityOrigin;
+        this.cityDestine = cityDestine;
+        this.distance = distance;
+    }
+
+    public Long getIdRoute() {
+        return IdRoute;
+    }
+
+    public void setIdRoute(Long IdRoute) {
+        this.IdRoute = IdRoute;
+    }
+
+    public String getCodeRoute() {
+        return codeRoute;
+    }
+
+    public void setCodeRoute(String codeRoute) {
+        this.codeRoute = codeRoute;
+    }
+
+    public String getCityOrigin() {
+        return cityOrigin;
+    }
+
+    public void setCityOrigin(String cityOrigin) {
+        this.cityOrigin = cityOrigin;
+    }
+
+    public String getCityDestine() {
+        return cityDestine;
+    }
+
+    public void setCityDestine(String cityDestine) {
+        this.cityDestine = cityDestine;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+/*
+    public Airport getIdAirportOrigin() {
+        return idAirportOrigin;
+    }
+
+    public void setIdAirportOrigin(Airport idAirportOrigin) {
+        this.idAirportOrigin = idAirportOrigin;
+    }
+
+    public Airport getIdAirportDestine() {
+        return idAirportDestine;
+    }
+
+    public void setIdAirportDestine(Airport idAirportDestine) {
+        this.idAirportDestine = idAirportDestine;
+    }
+    
+    
+    */
+
+    @Override
+    public String toString() {
+        return "Route{" + "IdRoute=" + IdRoute + ", codeRoute=" + codeRoute + ", cityOrigin=" + cityOrigin + ", cityDestine=" + cityDestine + ", distance=" + distance + '}';
+    }
+    
+    
     
 }
