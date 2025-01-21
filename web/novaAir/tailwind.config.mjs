c028419f87905060e07a85a1b1f2d0bb6af6de59
DLeonardoG <darkleonardog@gmail.com>
2025-01-21 04:58:08 -0500

    feat: :sparkles: It started with the development of the front part
    It started with the development of the front part
    
    By @DLeonardoG

--- web/novaAir/tailwind.config.mjs
+++ web/novaAir/tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'BlackSpecial': '#191414'
+		  }
 	},
 	plugins: [],
 }
