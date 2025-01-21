7ebdb896931c7cb6e53e5b6c05b69cf60505d53f
Kevinromero04 <kevinromerorincon7@gmail.com>
2025-01-21 12:18:31 -0500

    refactor: :sparkles: a new structural architecture was implemented
    this method the new structure we are going to use is a hexagonal one.
    
    By @Kevinromero04

---
+++ novaair/src/main/java/com/campus/novaair/crewmember/domain/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.campus.novaair.crewmember.domain;
+
+import com.campus.novaair.airport.application.*;
+
+/**
+ *
+ * @author kevin
+ */
+public class NewClass {
+    
+}
