397cec8baab6ec3cfd7b38a62c6f094659a0e2e5
DLeonardoG <darkleonardog@gmail.com>
2025-01-26 14:48:44 -0500

    feat: :zap: Added DTO for passenger
    Still incompleted
    
    By @DLeonardoG

---
+++ novaair/src/main/java/com/campus/novaair/passangers/domain/PassengerDTO.java
@@ -0,0 +1,48 @@
+
+package com.campus.novaair.passangers.domain;
+
+public class PassengerDTO {
+     private Long id;
+    private String name;
+    private String IDPassenger;
+
+    public PassengerDTO() {
+    }
+
+    public PassengerDTO(String name, String IDPassenger) {
+        this.name = name;
+        this.IDPassenger = IDPassenger;
+    }
+
+    public PassengerDTO(Long id, String name, String IDPassenger) {
+        this.id = id;
+        this.name = name;
+        this.IDPassenger = IDPassenger;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIDPassenger() {
+        return IDPassenger;
+    }
+
+    public void setIDPassenger(String IDPassenger) {
+        this.IDPassenger = IDPassenger;
+    }
+    
+    
+}
