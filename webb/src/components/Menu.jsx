83718f140d5061413ac5980605dd8e0c234e1617
DLeonardoG <darkleonardog@gmail.com>
2025-01-26 11:57:33 -0500

    feat: :sparkles: added full options to the menu
    Menu is completed
    
    By @DLeoanardoG

--- webb/src/components/Menu.jsx
+++ webb/src/components/Menu.jsx
@@ -4,17 +4,11 @@ import { AiFillHome, AiOutlineUnorderedList } from 'react-icons/ai';
 
 const Menu = () => {
   return (
-    <div className="w-64 h-screen bg-gray-800 text-white">
+    <div className="w-64 h-min-screen bg-gray-800 text-white">
       <div className="p-4">
-        <h1 className="text-3xl font-bold">Panel de Control</h1>
+        <h1 className="text-3xl font-bold">NovaAir</h1>
       </div>
       <ul>
-        <li className="p-4 hover:bg-gray-700">
-          <Link to="/airports" className="flex items-center">
-            <AiFillHome className="mr-2" />
-            Airports
-          </Link>
-        </li>
         <li className="p-4 hover:bg-gray-700">
           <Link to="/classseats" className="flex items-center">
             <AiOutlineUnorderedList className="mr-2" />
@@ -45,7 +39,43 @@ const Menu = () => {
             Crew Members
           </Link>
         </li>
-        {/* Añadir los demás enlaces de endpoints */}
+        <li className="p-4 hover:bg-gray-700">
+          <Link to="/airports" className="flex items-center">
+            <AiFillHome className="mr-2" />
+            Airports
+          </Link>
+        </li>
+        <li className="p-4 hover:bg-gray-700">
+          <Link to="/maintenances" className="flex items-center">
+            <AiFillHome className="mr-2" />
+            Maintenances
+          </Link>
+        </li>
+        <li className="p-4 hover:bg-gray-700">
+          <Link to="/passengers" className="flex items-center">
+            <AiFillHome className="mr-2" />
+            Passengers
+          </Link>
+        </li>
+        <li className="p-4 hover:bg-gray-700">
+          <Link to="/planes" className="flex items-center">
+            <AiFillHome className="mr-2" />
+            Planes
+          </Link>
+        </li>
+        <li className="p-4 hover:bg-gray-700">
+          <Link to="/tickets" className="flex items-center">
+            <AiFillHome className="mr-2" />
+            Tickets
+          </Link>
+        </li>
+        <li className="p-4 hover:bg-gray-700">
+          <Link to="/typesmaintenances" className="flex items-center">
+            <AiFillHome className="mr-2" />
+            Types Maintenances
+          </Link>
+        </li>
+        
       </ul>
     </div>
   );
