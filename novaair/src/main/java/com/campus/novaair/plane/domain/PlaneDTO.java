f7538f56f0843f8d51a24b873901a5dd1beb4079
DLeonardoG <darkleonardog@gmail.com>
2025-01-27 10:08:29 -0500

    feat: :sparkles: PlanDTO was created
    DTO
    
    By @DLeonardoG

--- novaair/src/main/java/com/campus/novaair/plane/domain/PlaneDTO.java
+++ novaair/src/main/java/com/campus/novaair/plane/domain/PlaneDTO.java
@@ -4,17 +4,21 @@ package com.campus.novaair.plane.domain;
 public class PlaneDTO {
     private Long id;
     private String model;
+    private String name;
     private Integer numSeat;
 
-    public PlaneDTO(Long id, String model, Integer numSeat) {
+    public PlaneDTO(Long id, String model, Integer numSeat, String name) {
         this.id = id;
         this.model = model;
         this.numSeat = numSeat;
+        this.name = name;
     }
 
-    public PlaneDTO(String model, Integer numSeat) {
+    public PlaneDTO(String model, Integer numSeat, String name) {
         this.model = model;
         this.numSeat = numSeat;
+        this.name = name;
+
     }
 
     public PlaneDTO() {
@@ -43,4 +47,13 @@ public class PlaneDTO {
     public void setNumSeat(Integer numSeat) {
         this.numSeat = numSeat;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
